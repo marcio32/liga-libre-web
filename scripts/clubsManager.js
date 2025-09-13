d321bae6ff5dd9a695539ed2c2857cb30da87f4e
Marcio Abriola <Marcioabriola@outlook.com>
2025-09-12 21:58:31 -0300

    Feat: Se comunica el front con la API

--- scripts/clubsManager.js
+++ scripts/clubsManager.js
@@ -1,22 +1,30 @@
-const url_base = `https://liga-libre-awdjddf5a6hpbrbe.brazilsouth-01.azurewebsites.net`;
-// {protocolo}://{hostname}:{puerto}
+function getAuthHeaders() {
+ const token = localStorage.getItem('token');
+ return {
+     'Content-Type': 'application/json',
+     'Authorization': `Bearer ${token}`
+ }
+}
 
-function loadClubs() {
+async function loadClubs() {
     // /{ruta del recurso}
-    fetch(url_base + '/club/all')
-        .then(response => {
-            return response.json();
-        })
-        .then(responseContent => {
-            // Acá entra cuando ya tenemos a disposición el resultado del HttpResponse
-            renderClubs(responseContent);
-        });
+    const response = await fetch(`${API_BASE_URL}/Club/GetAll`, {
+        headers: getAuthHeaders()
+    });
+
+    const responseContent = await response.json();
+
+    renderClubs(responseContent);
 }
 
 function renderClubs(clubs) {
     const tbody = document.querySelector('#clubsTable tbody')
+    const isAdmin = checkIsAdmin();
+
 
     clubs.forEach(club => {
+        const adminActions = isAdmin ? `<a class="btn btn-primary" href="#" onclick="editClub()">Editar</a> <a class="btn btn-danger" href="#" onclick="deleteClub()">Eliminar</a>` : '';
+
         const row = document.createElement('tr');
         row.innerHTML = `<td>${club.id}</td>
                          <td>${club.name}</td>
@@ -26,10 +34,64 @@ function renderClubs(clubs) {
                          <td>${club.phone}</td>
                          <td>${club.address}</td>
                          <td>${club.stadiumName}</td>
-                         <td><a href="www.google.com">ver</td>
+                         <td>
+                            <a class="btn btn-secondary" href="www.google.com">ver</a>
+                            ${adminActions} 
+                         </td>
                         `;
         tbody.appendChild(row);
     });
+}
+
+function checkIsAdmin(){
+    const roles = JSON.parse(localStorage.getItem('userRoles'));
+    return roles.includes('Admin');
+}
+
+function showCreateForm(){
+    const main = document.querySelector('.main-content');
+
+    main.innerHTML = `
+    <h2>Crear Club</h2>
+    <form id="clubForm">
+    <div style="width:15%">
+     <input class="form-control" type="text" id="name" name="name" placeholder="Nombre" required><br>
+        <input class="form-control" type="text" id="city" name="city" placeholder="Ciudad" required><br>
+        <input class="form-control" type="email" id="email" name="email" placeholder="Email" required><br>
+        <input class="form-control" type="number" id="numberOfPartners" name="numberOfPartners" placeholder="Número de socios" required><br>
+        <input class="form-control" type="text" id="phone" name="phone" placeholder="Teléfono" required><br>
+        <input class="form-control" type="text" id="address" name="address" placeholder="Dirección" required><br>
+        <input class="form-control" type="text" id="stadiumName" name="stadiumName" placeholder="Nombre del estadio" required><br>
+        <button class="btn btn-primary mt-3" type="button" onclick="createClub()">Crear</button>
+        <button class="btn btn-danger mt-3" type="button" onclick="location.reload()">Cancelar</button>
+    </div>
+       
+    </form>`
+}
+
+function createClub(){
+    const clubData = {
+        name: document.getElementById('name').value,
+        city: document.getElementById('city').value,
+        email: document.getElementById('email').value,
+        numberOfPartners: document.getElementById('numberOfPartners').value,
+        phone: document.getElementById('phone').value,
+        address: document.getElementById('address').value,
+        stadiumName: document.getElementById('stadiumName').value
+    };
+
+    fetch(`${API_BASE_URL}/Club/Create`, {
+        method: 'POST',
+        headers: getAuthHeaders(),
+        body: JSON.stringify(clubData)
+    }).then(response => {
+        if(response.ok){
+            alert('Club creado con éxito');
+            location.reload();
+        }else{
+            alert('Error al crear el club');
+        }
+    })
 }
 
 window.onload = () => loadClubs();
--- scripts/loginManager.js
+++ scripts/loginManager.js
@@ -1,15 +1,46 @@
-function login() {
-    const username = document.getElementById('username').value;
+const API_BASE_URL = "https://localhost:7007/api";
+
+async function login() {
+    const email = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (isLoginValid(username, password)) {
-        window.location.href = "index.html";
-    } else {
-        alert("Credenciales incorrectas");
+    if(!email || !password) {
+        alert("Por favor, complete todos los campos.");
+        return;
     }
+
+    try
+    {
+        const response = await fetch(`${API_BASE_URL}/auth/login`, {
+            method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify({email, password}) }
+    );
+
+    if(response.ok){
+        const data = await response.json();
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('userEmail', data.email);
+            localStorage.setItem('userRoles', JSON.stringify(data.roles));
+            window.location.href = 'index.html';
+    }else{
+        const error = await response.json();
+        alert('error:', error)
+    }
+
+    }
+    catch(error){
+        console.error('Error', error);
+        alert('Error de conexion. Intente nuevamente mas tarde.');
+    }
+
 }
 
-function isLoginValid(username, password) {
-    // Harcodeamos este if hasta que lo integremos con la API
-    return username === "user" && password === "123";
+function logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('userEmail');
+    localStorage.removeItem('userRoles');
+    window.location.href = 'login.html';
 }
